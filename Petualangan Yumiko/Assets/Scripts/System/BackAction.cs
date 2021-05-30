bf81fc82fdf7731296f1791c397ce1e8acb654c2
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2021-05-30 13:00:51 +0700

    google play

--- Petualangan Yumiko/Assets/Scripts/System/BackAction.cs
+++ Petualangan Yumiko/Assets/Scripts/System/BackAction.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class BackAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject exitPanel;
+    bool isShowing = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isShowing == true)
+        {
+            Time.timeScale = 0;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                isShowing = false;
+            }
+        }
+        else
+        {
+            Time.timeScale = 1;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                isShowing = true;
+            }
+        }
+
+        exitPanel.gameObject.SetActive(isShowing);
+    }
+
+    public void BackButtonPressed()
+    {
+        isShowing = false;
+    }
+
+    public void QuitButtonPressed()
+    {
+        isShowing = true;
     }
 }
